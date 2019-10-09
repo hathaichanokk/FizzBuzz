0479625ac8e7ba9dd33ad8b0da4a91ca2e656aad
paradee15 <pndendoo@gmail.com>
2019-10-09 17:14:37 +0700

    FizzBuzz Version3

--- src/testFizzBuzz5.java
+++ src/testFizzBuzz5.java
@@ -8,6 +8,7 @@ class testFizzBuzz5 {
 	FizzBuzz fizzBuzz = new FizzBuzz();
 	String i  = fizzBuzz.getString(1);
 	String i2  = fizzBuzz.getString(2);
+	String i3  = fizzBuzz.getString(2);
 	
 	@Test
 	void input_is_1_expect_1() {
@@ -18,5 +19,10 @@ class testFizzBuzz5 {
 	void input_is_2_expect_2() {
 		assertEquals(2, i2);
 	}
+	@Test
+	void input_is_3_expect_Fizz() {
+		assertEquals("Fizz", i3);
+	}
+
 
 }
