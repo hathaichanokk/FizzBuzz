26dd1b80e45dfb274029c09f14c0773c52b48a52
hathaichanokk <hathaichanokk@kkumail.com>
2019-10-09 17:09:52 +0700

    FizzBuzz2

--- src/testFizzBuzz5.java
+++ src/testFizzBuzz5.java
@@ -6,10 +6,17 @@ class testFizzBuzz5 {
 
 
 	FizzBuzz fizzBuzz = new FizzBuzz();
-	String i  = fizzBuzz.getString("FizzBuzz");
+	String i  = fizzBuzz.getString(1);
+	String i2  = fizzBuzz.getString(2);
+	
 	@Test
-	void input_is_15_expect_FizzBuzz() {
-		assertEquals("FizzBuzz", i);
+	void input_is_1_expect_1() {
+		assertEquals(1, i);
+	}
+	
+	@Test
+	void input_is_2_expect_2() {
+		assertEquals(2, i2);
 	}
 
 }
