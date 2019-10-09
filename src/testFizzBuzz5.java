40c77242cc63af7ffb5f510da2cd2cb5bbfc6375
hathaichanokk <hathaichanokk@kkumail.com>
2019-10-09 17:16:14 +0700

    FizzBuzz v4

--- src/testFizzBuzz5.java
+++ src/testFizzBuzz5.java
@@ -8,7 +8,8 @@ class testFizzBuzz5 {
 	FizzBuzz fizzBuzz = new FizzBuzz();
 	String i  = fizzBuzz.getString(1);
 	String i2  = fizzBuzz.getString(2);
-	String i3  = fizzBuzz.getString(2);
+	String i3  = fizzBuzz.getString(3);
+	String i4  = fizzBuzz.getString(4);
 	
 	@Test
 	void input_is_1_expect_1() {
@@ -22,6 +23,11 @@ class testFizzBuzz5 {
 	@Test
 	void input_is_3_expect_Fizz() {
 		assertEquals("Fizz", i3);
+	}
+	
+	@Test
+	void input_is_4_expect_4() {
+		assertEquals(4, i4);
 	}
 
 
